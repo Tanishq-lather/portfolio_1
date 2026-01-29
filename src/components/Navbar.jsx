0b9f34b7ecd6edb1b4656c27d7465b8b21674342
Tanishq lather <tanishqlather@gmail.com>
2026-01-29 11:32:04 +0530

    Responsiveness fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,15 +9,15 @@ export function Navbar() {
     <MotionFlowbiteNavbar
       fluid
       rounded
-      className="bg-[#FEA405]! shadow-md! text-[#1C352D]! fixed! top-0! left-0! right-0! z-10!"
+      className="bg-[#FEA405]! shadow-md text-[#1C352D]! fixed top-0 left-0 right-0 z-10"
       initial={{ opacity: 0, y: -50 }}
       transition={{ duration: 0.5 }}
       whileInView={{ opacity: 1, y: 0 }}
       viewport={{ once: true }}
     >
       <NavbarBrand href="/">
-        <Airplay className="mr-3! ml-3! h-6! sm:h-9!" />
-        <span className="self-center! whitespace-nowrap! text-xl! font-semibold!">Rahul</span>
+        <Airplay className="mr-3 ml-3 h-6 sm:h-9" />
+        <span className="self-center whitespace-nowrap text-xl font-semibold">Rahul</span>
       </NavbarBrand>
       <NavbarToggle className="text-[#1C352D]!" />
 
--- src/components/Pcard.jsx
+++ src/components/Pcard.jsx
@@ -3,14 +3,14 @@ import { Card } from "flowbite-react";
 export function Pcard(props) {
   return (
     <Card
-      className={`min-w-sm! w-60! md:w-full! ${props.className || ""}`}
+      className={`min-w-xs md:w-full ${props.className || ""}`}
       imgAlt="Meaningful alt text for an image that is not purely decorative"
       imgSrc={props.src}
     >
-      <h5 className="text-2xl! font-bold! tracking-tight text-gray-900!">
+      <h5 className="text-2xl font-bold tracking-tight text-gray-900">
         {props.title}
       </h5>
-      <p className="font-normal! text-gray-700!">
+      <p className="font-normal text-gray-700">
         {props.description}
       </p>
     </Card>
