054edd0e34503fa1154ec951a13ad97937c868e2
Tanishq lather <tanishqlather@gmail.com>
2026-01-29 21:53:42 +0530

    Responsiveness fix 2

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,22 +2,22 @@ import { Button, Checkbox, Label, TextInput } from "flowbite-react";
 
 export function Form() {
   return (
-    <form className="flex w-full flex-col bg-[#ffffff]! px-4 md:px-16 gap-4">
+    <form className="flex w-full flex-col bg-[#ffffff]! px-4 md:px-16 gap-4 shadow-md">
       <div>
         <div className="mb-2 block">
-          <Label htmlFor="email1">Your email</Label>
+          <Label className="text-[#1C352D]!" htmlFor="email1">Your email</Label>
         </div>
         <TextInput id="email1" type="email" placeholder="name@lather.com" required />
       </div>
       <div>
         <div className="mb-2 block">
-          <Label htmlFor="name1">Your name</Label>
+          <Label className="text-[#1C352D]!" htmlFor="name1">Your name</Label>
         </div>
         <TextInput id="name1" type="text" required />
       </div>
       <div className="flex items-center gap-2">
         <Checkbox id="remember" />
-        <Label htmlFor="remember">Remember me</Label>
+        <Label className="text-[#1C352D]!" htmlFor="remember">Remember me</Label>
       </div>
       <Button type="submit" className="bg-[#FEA405]! text-[#1C352D]! ">Submit</Button>
     </form>
